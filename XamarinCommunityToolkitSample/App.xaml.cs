9767dc6bcde9e5132cda09fc2b3770c17db191be
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2020-06-26 03:22:23 +0300

    updated range selector

--- XamarinCommunityToolkitSample/App.xaml.cs
+++ XamarinCommunityToolkitSample/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
 using XamarinCommunityToolkitSample.Pages;
 
 namespace XamarinCommunityToolkitSample
@@ -35,8 +36,14 @@ namespace XamarinCommunityToolkitSample
 
         RangeSelectorLayout Content { get; set; }
 
-        class RangeSelectorLayout : AbsoluteLayout
+        sealed class RangeSelectorLayout : AbsoluteLayout
         {
+            readonly Dictionary<View, double> thumbPositionMap = new Dictionary<View, double>();
+
+            double MinValue { get; set; } = 0;
+
+            double MaxValue { get; set; } = .5;
+
             Frame Track { get; } = new Frame
             {
                 Padding = 0,
@@ -65,16 +72,41 @@ namespace XamarinCommunityToolkitSample
 
             public RangeSelectorLayout()
             {
-                SetLayoutFlags(Track, AbsoluteLayoutFlags.YProportional | AbsoluteLayoutFlags.WidthProportional);
-                SetLayoutFlags(SelectedTrack, AbsoluteLayoutFlags.YProportional | AbsoluteLayoutFlags.WidthProportional);
-                SetLayoutFlags(MinThumb, AbsoluteLayoutFlags.PositionProportional | AbsoluteLayoutFlags.HeightProportional);
-                SetLayoutFlags(MaxThumb, AbsoluteLayoutFlags.PositionProportional | AbsoluteLayoutFlags.HeightProportional);
+                AddGesture(MinThumb);
+                AddGesture(MaxThumb);
 
-                Children.Add(Track);
-                Children.Add(SelectedTrack);
-                Children.Add(MinThumb);
-                Children.Add(MaxThumb);
-                Update();
+                AddChild(Track);
+                AddChild(SelectedTrack);
+                AddChild(MinThumb);
+                AddChild(MaxThumb);
+
+                UpdateLayot();
+            }
+
+            public void UpdateLayot()
+            {
+                var width = Width;
+                var height = Height;
+                if (width < 0 || height < 0)
+                    return;
+
+                var trackHeight = height / 10;
+                var trackRadius = (float)trackHeight / 2;
+                var thumbSize = height;
+                var thumbRadius = (float)thumbSize / 2;
+
+                Track.CornerRadius = trackRadius;
+                SelectedTrack.CornerRadius = trackRadius;
+                MinThumb.CornerRadius = thumbRadius;
+                MaxThumb.CornerRadius = thumbRadius;
+
+                var minValue = MinValue * width;
+                var maxValue = MaxValue * width;
+
+                SetLayoutBounds(Track, new Rectangle(0, .5, width, trackHeight));
+                SetLayoutBounds(SelectedTrack, new Rectangle(minValue, .5, maxValue - minValue, trackHeight));
+                SetLayoutBounds(MinThumb, new Rectangle(minValue, .5, thumbSize, thumbSize));
+                SetLayoutBounds(MaxThumb, new Rectangle(maxValue, .5, thumbSize, thumbSize));
             }
 
             protected override void OnParentSet()
@@ -86,27 +118,67 @@ namespace XamarinCommunityToolkitSample
             protected override void OnSizeAllocated(double width, double height)
             {
                 base.OnSizeAllocated(width, height);
-                Update();
+                UpdateLayot();
             }
 
-            // min & max values
-
-            void Update()
+            void AddChild(View view)
             {
-                if (Height < 0 || Width < 0)
-                    return;
+                SetLayoutFlags(view, AbsoluteLayoutFlags.YProportional);
+                Children.Add(view);
+            }
 
-                var trackHeight = Height / 15;
-                var trackRadius = (float)trackHeight / 2;
-                var thumbRadius = (float)Height / 2;
-                Track.CornerRadius = trackRadius;
-                SelectedTrack.CornerRadius = trackRadius;
-                MinThumb.CornerRadius = thumbRadius;
-                MaxThumb.CornerRadius = thumbRadius;
-                SetLayoutBounds(Track, new Rectangle(0, .5, 1, trackHeight));
-                SetLayoutBounds(SelectedTrack, new Rectangle(0, .5, .5, trackHeight));
-                SetLayoutBounds(MinThumb, new Rectangle(0, 0, Height, 1));
-                SetLayoutBounds(MaxThumb, new Rectangle(.5, 0, Height, 1));
+            void AddGesture(View view)
+            {
+                var gesture = new PanGestureRecognizer();
+                gesture.PanUpdated += OnPanUpdated;
+                view.GestureRecognizers.Add(gesture);
+            }
+
+            void OnPanUpdated(object sender, PanUpdatedEventArgs e)
+            {
+                var view = (View)sender;
+                switch (e.StatusType)
+                {
+                    case GestureStatus.Started:
+                        OnPanStarted(view);
+                        break;
+                    case GestureStatus.Running:
+                        OnPanRunning(view, e.TotalX);
+                        break;
+                    case GestureStatus.Completed:
+                        OnPanCompleted(view);
+                        break;
+                    case GestureStatus.Canceled:
+                        OnPanCanceled(view);
+                        break;
+                }
+            }
+
+            void OnPanStarted(View view)
+                => thumbPositionMap[view] = GetLayoutBounds(view).X;
+
+            void OnPanRunning(View view, double value)
+            {
+                SetValue(view, value + thumbPositionMap[view]);
+                UpdateLayot();
+            }
+
+            void OnPanCompleted(View view)
+            {
+            }
+
+            void OnPanCanceled(View view)
+                => SetValue(view, thumbPositionMap[view]);
+
+            void SetValue(View view, double value)
+            {
+                value /= Width;
+                if (view == MinThumb)
+                {
+                    MinValue = value;
+                    return;
+                }
+                MaxValue = value;
             }
         }
     }
